rails/rails

commit 51bef9d8fb0b4da7a104425ab8545e9331387743

to_xml should also rely on serializable hash.

activemodel/lib/active_model/serializers/xml.rb
@@ -15,10 +15,10 @@ module ActiveModel
         class Attribute #:nodoc:
           attr_reader :name, :value, :type
 
-          def initialize(name, serializable, raw_value=nil)
+          def initialize(name, serializable, value)
             @name, @serializable = name, serializable
-            raw_value = raw_value.in_time_zone if raw_value.respond_to?(:in_time_zone)
-            @value = raw_value || @serializable.send(name)
+            value  = value.in_time_zone if value.respond_to?(:in_time_zone)
+            @value = value
             @type  = compute_type
           end
 
@@ -49,38 +49,22 @@ module ActiveModel
         def initialize(serializable, options = nil)
           @serializable = serializable
           @options = options ? options.dup : {}
-
-          @options[:only] = Array.wrap(@options[:only]).map { |n| n.to_s }
-          @options[:except] = Array.wrap(@options[:except]).map { |n| n.to_s }
         end
 
-        # To replicate the behavior in ActiveRecord#attributes, <tt>:except</tt>
-        # takes precedence over <tt>:only</tt>. If <tt>:only</tt> is not set
-        # for a N level model but is set for the N+1 level models,
-        # then because <tt>:except</tt> is set to a default value, the second
-        # level model can have both <tt>:except</tt> and <tt>:only</tt> set. So if
-        # <tt>:only</tt> is set, always delete <tt>:except</tt>.
-        def attributes_hash
-          attributes = @serializable.attributes
-          if options[:only].any?
-            attributes.slice(*options[:only])
-          elsif options[:except].any?
-            attributes.except(*options[:except])
-          else
-            attributes
+        def serializable_hash
+          @serializable.serializable_hash(@options.except(:include))
+        end
+
+        def serializable_collection
+          methods = Array.wrap(options[:methods]).map(&:to_s)
+          serializable_hash.map do |name, value|
+            name = name.to_s
+            if methods.include?(name)
+              self.class::MethodAttribute.new(name, @serializable, value)
+            else
+              self.class::Attribute.new(name, @serializable, value)
+            end
           end
-        end
-
-        def serializable_attributes
-          attributes_hash.map do |name, value|
-            self.class::Attribute.new(name, @serializable, value)
-          end
-        end
-
-        def serializable_methods
-          Array.wrap(options[:methods]).map do |name|
-            self.class::MethodAttribute.new(name.to_s, @serializable) if @serializable.respond_to?(name.to_s)
-          end.compact
         end
 
         def serialize
@@ -114,7 +98,7 @@ module ActiveModel
         end
 
         def add_attributes_and_methods
-          (serializable_attributes + serializable_methods).each do |attribute|
+          serializable_collection.each do |attribute|
             key = ActiveSupport::XmlMini.rename_key(attribute.name, options)
             ActiveSupport::XmlMini.to_tag(key, attribute.value,
               options.merge(attribute.decorations))

activemodel/test/cases/serializers/xml_serialization_test.rb
@@ -33,6 +33,12 @@ class Address
   end
 end
 
+class SerializableContact < Contact
+  def serializable_hash(options={})
+    super(options.merge(:only => [:name, :age]))
+  end
+end
+
 class XmlSerializationTest < ActiveModel::TestCase
   def setup
     @contact = Contact.new
@@ -94,6 +100,17 @@ class XmlSerializationTest < ActiveModel::TestCase
     assert_match %r{^<xmlContact>},  @xml
     assert_match %r{</xmlContact>$}, @xml
     assert_match %r{<createdAt},     @xml
+  end
+
+  test "should use serialiable hash" do
+    @contact = SerializableContact.new
+    @contact.name = 'aaron stack'
+    @contact.age = 25
+
+    @xml = @contact.to_xml
+    assert_match %r{<name>aaron stack</name>}, @xml
+    assert_match %r{<age type="integer">25</age>}, @xml
+    assert_no_match %r{<awesome>}, @xml
   end
 
   test "should allow skipped types" do

activerecord/lib/active_record/serializers/xml_serializer.rb
@@ -179,7 +179,7 @@ module ActiveRecord #:nodoc:
   class XmlSerializer < ActiveModel::Serializers::Xml::Serializer #:nodoc:
     def initialize(*args)
       super
-      options[:except] |= Array.wrap(@serializable.class.inheritance_column)
+      options[:except] = Array.wrap(options[:except]) | Array.wrap(@serializable.class.inheritance_column)
     end
 
     class Attribute < ActiveModel::Serializers::Xml::Serializer::Attribute #:nodoc:

rails/activeresource

commit 1b98f0988dbf64a3922ded3c4e361d346cb78cac

Make serializable_hash take attr values directly from attributes hash.
Previously, it would use send() to get the attribute. In Active
Resource, this would rely on hitting method missing. If a method with
the same name was defined further up the ancestor chain, that method
would wrongly be called.

This change fixes test_to_xml_with_private_method_name_as_attribute in
activeresource/test/cases/base_test.rb, which was broken after
51bef9d8fb0b4da7a104425ab8545e9331387743, because that change made
to_xml use serializable_hash.

test/cases/base_test.rb
@@ -1004,9 +1004,17 @@ class BaseTest < Test::Unit::TestCase
 
   def test_to_xml_with_private_method_name_as_attribute
     Person.format = :xml
-    assert_nothing_raised(ArgumentError) {
-      Customer.new(:test => true).to_xml
-    }
+
+    customer = Customer.new(:foo => "foo")
+    customer.singleton_class.class_eval do
+      def foo
+        "bar"
+      end
+      private :foo
+    end
+
+    assert !customer.to_xml.include?("<foo>bar</foo>")
+    assert customer.to_xml.include?("<foo>foo</foo>")
   ensure
     Person.format = :json
   end