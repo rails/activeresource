40fbf4f11d682eb5cf577e3082ce7146af39c213
Peter McCracken <peter.mccracken@jadedpixel.com>
2014-02-22 16:41:26 -0500

    adding an extra test for editing a threadsafe attribute

--- test/threadsafe_attributes_test.rb
+++ test/threadsafe_attributes_test.rb
@@ -9,7 +9,7 @@ class ThreadsafeAttributesTest < ActiveSupport::TestCase
     @tester = TestClass.new
   end
 
-  test "#threadsafe attributes work in a single thread" do
+  test "threadsafe attributes work in a single thread" do
     refute @tester.safeattr_defined?
     assert_nil @tester.safeattr
     @tester.safeattr = "a value"
@@ -17,7 +17,7 @@ class ThreadsafeAttributesTest < ActiveSupport::TestCase
     assert_equal "a value", @tester.safeattr
   end
 
-  test "#threadsafe attributes inherit the value of the main thread" do
+  test "threadsafe attributes inherit the value of the main thread" do
     @tester.safeattr = "a value"
     Thread.new do
       assert @tester.safeattr_defined?
@@ -26,13 +26,22 @@ class ThreadsafeAttributesTest < ActiveSupport::TestCase
     assert_equal "a value", @tester.safeattr
   end
 
-  test "#changing a threadsafe attribute in a thread does not affect the main thread" do
+  test "writing a threadsafe attribute in a thread does not affect the main thread" do
     @tester.safeattr = "a value"
     Thread.new do
       @tester.safeattr = "a new value"
       assert_equal "a new value", @tester.safeattr
     end.join
     assert_equal "a value", @tester.safeattr
+  end
+
+  test "modifying a threadsafe attribute in a thread does not affect the main thread" do
+    @tester.safeattr = {a: 1}
+    Thread.new do
+      @tester.safeattr[:a] = 2
+      assert_equal 2, @tester.safeattr[:a]
+    end.join
+    assert_equal 1, @tester.safeattr[:a]
   end
 
 end
