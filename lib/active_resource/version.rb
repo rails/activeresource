761cb48c7c5d2f33da8bd6b6582a057bf31dd735
Peter McCracken <peter.mccracken@jadedpixel.com>
2015-01-06 19:41:12 +0000

    update the version

--- lib/active_resource/version.rb
+++ lib/active_resource/version.rb
@@ -1,9 +1,12 @@
 module ActiveResource
   module VERSION #:nodoc:
+    # keeping this in lockstep with Rails version numbers
     MAJOR = 4
-    MINOR = 0
+    MINOR = 2
     TINY  = 0
-    PRE   = nil
+
+    # the PRE string is used to detect that we're using threadsafe in other gems
+    PRE  = 'threadsafe'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
