d8074a73efd0d475876e0465189f0364f97d74dd
Fernando Chucre <fernandochucre@gmail.com>
2015-12-17 09:49:01 -0200

    add parser callbacks with [before, around, after] hooks

--- lib/active_resource/callbacks.rb
+++ lib/active_resource/callbacks.rb
@@ -7,14 +7,15 @@ module ActiveResource
     CALLBACKS = [
       :before_validation, :after_validation, :before_save, :around_save, :after_save,
       :before_create, :around_create, :after_create, :before_update, :around_update,
-      :after_update, :before_destroy, :around_destroy, :after_destroy
+      :after_update, :before_destroy, :around_destroy, :after_destroy, :before_parser,
+      :around_parser, :after_parser
     ]
 
     included do
       extend ActiveModel::Callbacks
       include ActiveModel::Validations::Callbacks
 
-      define_model_callbacks :save, :create, :update, :destroy
+      define_model_callbacks :save, :create, :update, :destroy, :parser
     end
   end
 end
