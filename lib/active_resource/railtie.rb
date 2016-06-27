6c4c02b77ea04060b02bf023d93fd0e076756427
Rafael Mendonça França <rafaelmfranca@gmail.com>
2016-06-27 13:00:54 -0300

    Remove observers from the railtie
    This was moved to the rails-observers gem
    
    Fixes #221

--- lib/active_resource/railtie.rb
+++ lib/active_resource/railtie.rb
@@ -10,18 +10,6 @@ module ActiveResource
         ActiveResource::Base.send "#{k}=", v
       end
     end
-
-    config.after_initialize do |app|
-      ActiveSupport.on_load(:active_resource) do
-        ActiveResource::Base.instantiate_observers
-
-        # Rails 5.1 forward-compat. AD::R is deprecated to AS::R in Rails 5.
-        reloader = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Reloader
-        reloader.to_prepare do
-          ActiveResource::Base.instantiate_observers
-        end
-      end
-    end
   end
 end
 
