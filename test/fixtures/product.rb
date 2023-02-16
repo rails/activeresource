0bcabfb30c04546df3ae99e9c9e5a88c7b9ae9d5
Phil Murray <pmurray@nevada.net.nz>
2023-02-17 12:57:38 +1300

    Add tests to ensure subclasses inherit the headers from their superclass

--- test/fixtures/product.rb
+++ test/fixtures/product.rb
@@ -2,4 +2,10 @@
 
 class Product < ActiveResource::Base
   self.site = "http://37s.sunrise.i:3000"
+  # X-Inherited-Header is for testing that any subclasses
+  # include the headers of this class
+  self.headers["X-Inherited-Header"] = 'present'
+end
+
+class SubProduct < Product
 end
