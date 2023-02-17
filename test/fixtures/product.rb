47fd8a0396372a6e11b5b5f816b685f2060cce40
Phil Murray <pmurray@nevada.net.nz>
2023-02-17 14:11:14 +1300

    Rubocop fixes

--- test/fixtures/product.rb
+++ test/fixtures/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveResource::Base
   self.site = "http://37s.sunrise.i:3000"
   # X-Inherited-Header is for testing that any subclasses
   # include the headers of this class
-  self.headers["X-Inherited-Header"] = 'present'
+  self.headers["X-Inherited-Header"] = "present"
 end
 
 class SubProduct < Product
