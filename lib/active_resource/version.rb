9c8a2ee30325ff0bf789a239cc25fa361ec4a887
Jean Boussier <jean.boussier@gmail.com>
2024-10-31 18:49:59 +0100

    Release 6.1.4

--- lib/active_resource/version.rb
+++ lib/active_resource/version.rb
@@ -4,7 +4,7 @@ module ActiveResource
   module VERSION # :nodoc:
     MAJOR = 6
     MINOR = 1
-    TINY  = 3
+    TINY  = 4
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
