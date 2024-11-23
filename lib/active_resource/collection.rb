b570702d13ef267f410b385ffad027c8dfe7342d
Jean Luis Urena <jlurena@teladochealth.com>
2024-11-23 18:05:12 -0500

    [ISSUE-408] Refactored Collection
    Simplified delegation of Array, added docs

--- lib/active_resource/collection.rb
+++ lib/active_resource/collection.rb
@@ -5,11 +5,9 @@ require "active_support/inflector"
 
 module ActiveResource # :nodoc:
   class Collection # :nodoc:
-    SELF_DEFINE_METHODS = [:to_a, :all?]
     include Enumerable
-    delegate :to_yaml, :all?, *(Array.instance_methods(false) - SELF_DEFINE_METHODS), to: :to_a
+    delegate :to_yaml, *Array.public_instance_methods(false), to: :fetch_resources!
 
-    # The array of actual elements returned by index actions
     attr_accessor :elements, :resource_class, :query_params, :path_params
     attr_writer :prefix_options
     attr_reader :from
@@ -65,56 +63,92 @@ module ActiveResource # :nodoc:
       parse_response(@elements) if @elements.present?
     end
 
+    # Processes and sets the collection elements. This method assigns the provided `elements`
+    # (or an empty array if none provided) to the `@elements` instance variable.
+    #
+    # ==== Arguments
+    #
+    # +elements+ (Array<Object>) - An optional array of resources to be set as the collection elements.
+    #                              Defaults to an empty array.
+    #
+    # This method is called after fetching the resource and can be overridden by subclasses to
+    # handle any specific response format of the API.
     def parse_response(elements)
       @elements = elements || []
     end
 
+    # Returns the prefix options for the collection, which are used for constructing the resource path.
+    #
+    # ==== Returns
+    #
+    # [Hash] The prefix options for the collection.
     def prefix_options
       @prefix_options || {}
     end
 
-    # Makes network request to get the elements and returns self
+    # Executes the request to fetch the collection of resources from the API and returns the collection.
+    #
+    # ==== Returns
+    #
+    # [ActiveResource::Collection] The collection of resources.
     def call
-      to_a
+      fetch_resources!
       self
     end
 
-    def to_a
-      response =
-        case from
-        when Symbol
-          resource_class.get(from, path_params)
-        when String
-          path = "#{from}#{query_string(query_params)}"
-          resource_class.format.decode(resource_class.connection.get(path, resource_class.headers).body)
-        else
-          path = resource_class.collection_path(prefix_options, query_params)
-          resource_class.format.decode(resource_class.connection.get(path, resource_class.headers).body)
-        end
-
-      # Update the elements
-      parse_response(response)
-      @elements = @elements.map do |element|
-        resource_class.instantiate_record(element, prefix_options)
-      end
-    rescue ActiveResource::ResourceNotFound
-      # Swallowing ResourceNotFound exceptions and return nothing - as per ActiveRecord.
-      # Needs to be empty array as Array methods are delegated
-      []
-    end
-
+    # Returns the first resource in the collection, or creates a new resource using the provided
+    # attributes if the collection is empty.
+    #
+    # ==== Arguments
+    #
+    # +attributes+ (Hash) - The attributes for creating the resource.
+    #
+    # ==== Returns
+    #
+    # [Object] The first resource, or a newly created resource if none exist.
+    #
+    # ==== Example
+    #   post = PostCollection.where(title: "New Post").first_or_create
+    #   # => Post instance with title "New Post"
     def first_or_create(attributes = {})
       first || resource_class.create(query_params.update(attributes))
     rescue NoMethodError
       raise "Cannot create resource from resource type: #{resource_class.inspect}"
     end
 
+    # Returns the first resource in the collection, or initializes a new resource using the provided
+    # attributes if the collection is empty.
+    #
+    # ==== Arguments
+    #
+    # +attributes+ (Hash) - The attributes for initializing the resource.
+    #
+    # ==== Returns
+    #
+    # [Object] The first resource, or a newly initialized resource if none exist.
+    #
+    # ==== Example
+    #   post = PostCollection.where(title: "New Post").first_or_initialize
+    #   # => Post instance with title "New Post"
     def first_or_initialize(attributes = {})
       first || resource_class.new(query_params.update(attributes))
     rescue NoMethodError
       raise "Cannot build resource from resource type: #{resource_class.inspect}"
     end
 
+    # Filters the collection based on the provided clauses (query parameters).
+    #
+    # ==== Arguments
+    #
+    # +clauses+ (Hash) - A hash of query parameters used to filter the collection.
+    #
+    # ==== Returns
+    #
+    # [ActiveResource::Collection] A new collection filtered by the specified clauses.
+    #
+    # ==== Example
+    #   filtered_posts = PostCollection.where(title: "Post 1")
+    #   # => PostCollection:xxx (filtered collection)
     def where(clauses = {})
       raise ArgumentError, "expected a clauses Hash, got #{clauses.inspect}" unless clauses.is_a? Hash
       new_clauses = query_params.merge(clauses)
@@ -124,6 +158,34 @@ module ActiveResource # :nodoc:
     private
       def query_string(options)
         "?#{options.to_query}" unless options.nil? || options.empty?
+      end
+
+      # Fetches resources from the API and parses the response. The resources are then mapped to their respective
+      # resource class instances.
+      #
+      # ==== Returns
+      #
+      # [Array<Object>] The collection of resources retrieved from the API.
+      def fetch_resources!
+        response =
+          case from
+          when Symbol
+            resource_class.get(from, path_params)
+          when String
+            path = "#{from}#{query_string(query_params)}"
+            resource_class.format.decode(resource_class.connection.get(path, resource_class.headers).body)
+          else
+            path = resource_class.collection_path(prefix_options, query_params)
+            resource_class.format.decode(resource_class.connection.get(path, resource_class.headers).body)
+          end
+
+        # Update the elements
+        parse_response(response)
+        @elements.map! { |e| resource_class.instantiate_record(e, prefix_options) }
+      rescue ActiveResource::ResourceNotFound
+        # Swallowing ResourceNotFound exceptions and return nothing - as per ActiveRecord.
+        # Needs to be empty array as Array methods are delegated
+        []
       end
   end
 end
