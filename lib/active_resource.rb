072972f89d65bb158075128266857c65401b4544
Sean Doyle <sean.p.doyle24@gmail.com>
2025-01-25 16:23:01 -0500

    Integrate with Active Record's `.serialize`
    Define `ActiveResource::Base.dump` and `ActiveResource::Base.load` to
    support passing classes directly to [serialize][] as the `:coder`
    option:
    
    Writing to String columns
    ---
    
    Encodes Active Resource instances into a string to be stored in the
    database. Decodes strings read from the database into Active Resource
    instances.
    
    ```ruby
    class User < ActiveRecord::Base
      serialize :person, coder: Person
    end
    
    class Person < ActiveResource::Base
      schema do
        attribute :name, :string
      end
    end
    
    user = User.new
    user.person = Person.new name: "Matz"
    user.person_before_type_cast # => "{\"name\":\"Matz\"}"
    ```
    
    Writing string values incorporates the Base.format:
    
    ```ruby
    Person.format = :xml
    
    user.person = Person.new name: "Matz"
    user.person_before_type_cast # => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><person><name>Matz</name></person>"
    ```
    
    Instances are loaded as persisted when decoded from data containing a
    primary key value, and new records when missing a primary key value:
    
    ```ruby
    user.person = Person.new
    user.person.persisted? # => false
    
    user.person = Person.find(1)
    user.person.persisted? # => true
    ```
    
    Writing to JSON and JSONB columns
    ---
    
    ```ruby
    class User < ActiveRecord::Base
      serialize :person, coder: ActiveResource::Coder.new(Person, :serializable_hash)
    end
    
    class Person < ActiveResource::Base
      schema do
        attribute :name, :string
      end
    end
    
    user = User.new
    user.person = Person.new name: "Matz"
    user.person_before_type_cast # => {"name"=>"Matz"}
    
    user.person.name # => "Matz"
    ```
    
    The `ActiveResource::Coder` class
    ===
    
    By default, `#dump` serializes the instance to a string value by
    calling `ActiveResource::Base#encode`:
    
    ```ruby
    user.person_before_type_cast # => "{\"name\":\"Matz\"}"
    ```
    
    To customize serialization, pass the method name or a block as the second
    argument:
    
    ```ruby
    person = Person.new name: "Matz"
    
    coder = ActiveResource::Coder.new(Person, :serializable_hash)
    coder.dump(person) # => {"name"=>"Matz"}
    
    coder = ActiveResource::Coder.new(Person) { |person| person.serializable_hash }
    coder.dump(person) # => {"name"=>"Matz"}
    ```
    
    [serialize]: https://api.rubyonrails.org/classes/ActiveRecord/AttributeMethods/Serialization/ClassMethods.html#method-i-serialize

--- lib/active_resource.rb
+++ lib/active_resource.rb
@@ -37,11 +37,13 @@ module ActiveResource
 
   autoload :Base
   autoload :Callbacks
+  autoload :Coder
   autoload :Connection
   autoload :CustomMethods
   autoload :Formats
   autoload :HttpMock
   autoload :Schema
+  autoload :Serialization
   autoload :Singleton
   autoload :InheritingHash
   autoload :Validations
