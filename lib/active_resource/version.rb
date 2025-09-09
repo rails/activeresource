0e47ca6ce9199a8067aa1ff21649ee29d1ac3626
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2025-09-09 15:33:04 +0100

    Bump version from 6.1.4 to 6.1.5
    To capture the warning fix in https://github.com/rails/activeresource/pull/427/files

--- lib/active_resource/version.rb
+++ lib/active_resource/version.rb
@@ -4,7 +4,7 @@ module ActiveResource
   module VERSION # :nodoc:
     MAJOR = 6
     MINOR = 1
-    TINY  = 4
+    TINY  = 5
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
