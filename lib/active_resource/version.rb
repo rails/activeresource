3831ceb41c2b7717266ac751ebbb62d9576591c3
Rafael Mendonça França <rafael@rubyonrails.org>
2025-09-11 16:32:26 +0000

    Prepare for 6.2.0

--- lib/active_resource/version.rb
+++ lib/active_resource/version.rb
@@ -3,8 +3,8 @@
 module ActiveResource
   module VERSION # :nodoc:
     MAJOR = 6
-    MINOR = 1
-    TINY  = 4
+    MINOR = 2
+    TINY  = 0
     PRE   = nil
 
     STRING = [ MAJOR, MINOR, TINY, PRE ].compact.join(".")
