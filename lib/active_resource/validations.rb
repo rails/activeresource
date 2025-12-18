a4ef2aae4e4736e59f1987b3f640794d5e2a09a5
Sean Doyle <sean.p.doyle24@gmail.com>
2025-12-18 14:35:16 -0500

    Validations: Default context based on persistence
    This commit proposed that Active Resource follow the same pattern as
    [Active Record Validations][], namely that an instance's default
    validation context will be `:create` when the record **is not yet**
    persisted and `:update` when the record **is** persisted.
    
    [Active Record Validations]: https://api.rubyonrails.org/v8.1/classes/ActiveRecord/Validations.html#method-i-valid-3F

--- lib/active_resource/validations.rb
+++ lib/active_resource/validations.rb
@@ -314,6 +314,14 @@ module ActiveResource
     # saved.
     # Remote errors can only be cleared by trying to re-save the resource.
     #
+    # If the argument is +false+ (default is +nil+), the context is set to <tt>:create</tt> if
+    # {new_record?}[rdoc-ref:Base#new_record?] is +true+, and to <tt>:update</tt> if it is not.
+    # If the argument is an array of contexts, <tt>post.valid?([:create, :update])</tt>, the validations are
+    # run within multiple contexts.
+    #
+    # \Validations with no <tt>:on</tt> option will run no matter the context. \Validations with
+    # some <tt>:on</tt> option will only run in the specified context.
+    #
     # ==== Examples
     #   my_person = Person.create(params[:person])
     #   my_person.valid?
@@ -324,6 +332,8 @@ module ActiveResource
     #   # => false
     #
     def valid?(context = nil)
+      context ||= new_record? ? :create : :update
+
       run_callbacks :validate do
         super
         load_remote_errors(@remote_errors, true) if defined?(@remote_errors) && @remote_errors.present?
